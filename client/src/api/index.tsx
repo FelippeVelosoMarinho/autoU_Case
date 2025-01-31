63913d756f151aea33491bd2f361b0d217ef2f23
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2025-01-31 12:33:34 -0300

    fix: baseURL to railwail

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:8001",
-    baseURL: "https://apiautoucase-production.up.railway.app/,"
+    baseURL: "https://apiautoucase-production.up.railway.app"
   });
