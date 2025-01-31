202793095218c9df3504956ded136c8ecf2c8755
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2025-01-31 11:24:00 -0300

    feat: creating docker-compose and tests deploy

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8001",
+    // baseURL: "http://localhost:8001",
+    baseURL: "https://fastapi-production-e543.up.railway.app,"
   });
