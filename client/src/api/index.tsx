176043dbf95c36e59ded1e77180ab5f87efb5ecb
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2025-01-31 15:49:42 -0300

    fix: baseURL deploied api

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     //baseURL: "http://localhost:8001",
-    baseURL: import.meta.env.API_URL,
+    baseURL: "https://apiautoucase-production.up.railway.app",
   });
