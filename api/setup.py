beff66c5aa050b30151e564259c2a524b88cc602
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2025-01-31 11:42:57 -0300

    fix: config to deploy

--- api/setup.py
+++ api/setup.py
@@ -4,12 +4,22 @@ setup(
     name="autoU_case",
     version="0.1.0",
     description="Uma aplicação FastAPI de exemplo",
-    author="Seu Nome",
-    author_email="seu.email@example.com",
+    author="Felippe Veloso",
+    author_email="felippe.veloso15@gmail.com",
     packages=find_packages(),  # Inclui automaticamente os pacotes no diretório
     install_requires=[
         "fastapi==0.115.7",
-        "uvicorn==0.34.0"
+        "uvicorn==0.34.0",
+        "requests",
+        "python-multipart",
+        "transformers==4.48.1",
+        "torch==2.6.0",
+        "datasets==3.2.0",
+        "python-dotenv==1.0.1",
+        "PyPDF2==3.0.1",
+        "huggingface_hub==0.28.1",
+        "hypercorn==0.13.1 ",
+        "pytest==7.1.2"
     ],
     entry_points={
         "console_scripts": [
